2974108425907aefdeed0f8846f59ed87975211b
angelsilalahi <atitiandes12@gmail.com>
2021-01-28 11:34:58 +0700

    Update landing_template.blade.php

--- resources/views/landing_template.blade.php
+++ resources/views/landing_template.blade.php
@@ -9,18 +9,18 @@
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	
-	<link rel="stylesheet" href="{{asset('salvation/')}}/css/animate.css">
+	<link rel="stylesheet" href="{{asset('salvation/css/animate.css')}}">
 	
-	<link rel="stylesheet" href="{{asset('salvation/')}}/css/owl.carousel.min.css">
-	<link rel="stylesheet" href="{{asset('salvation/')}}/css/owl.theme.default.min.css">
-	<link rel="stylesheet" href="{{asset('salvation/')}}/css/magnific-popup.css">
+	<link rel="stylesheet" href="{{asset('salvation/css/owl.carousel.min.css')}}">
+	<link rel="stylesheet" href="{{asset('salvation/css/owl.theme.default.min.css')}}">
+	<link rel="stylesheet" href="{{asset('salvation/css/magnific-popup.css')}}">
 
 
-	<link rel="stylesheet" href="{{asset('salvation/')}}/css/bootstrap-datepicker.css">
-	<link rel="stylesheet" href="{{asset('salvation/')}}/css/jquery.timepicker.css">
+	<link rel="stylesheet" href="{{asset('salvation/css/bootstrap-datepicker.css')}}">
+	<link rel="stylesheet" href="{{asset('salvation/css/jquery.timepicker.css')}}">
 
-	<link rel="stylesheet" href="{{asset('salvation/')}}/css/flaticon.css">
-	<link rel="stylesheet" href="{{asset('salvation/')}}/css/style.css">
+	<link rel="stylesheet" href="{{asset('salvation/css/flaticon.css')}}">
+	<link rel="stylesheet" href="{{asset('salvation/css/style.css')}}">
 </head>
 <body>
 
@@ -145,22 +145,22 @@
 		<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
-		<script src="{{asset('salvation/')}}/js/jquery.min.js"></script>
-		<script src="{{asset('salvation/')}}/js/jquery-migrate-3.0.1.min.js"></script>
-		<script src="{{asset('salvation/')}}/js/popper.min.js"></script>
-		<script src="{{asset('salvation/')}}/js/bootstrap.min.js"></script>
-		<script src="{{asset('salvation/')}}/js/jquery.easing.1.3.js"></script>
-		<script src="{{asset('salvation/')}}/js/jquery.waypoints.min.js"></script>
-		<script src="{{asset('salvation/')}}/js/jquery.stellar.min.js"></script>
-		<script src="{{asset('salvation/')}}/js/jquery.animateNumber.min.js"></script>
-		<script src="{{asset('salvation/')}}/js/bootstrap-datepicker.js"></script>
-		<script src="{{asset('salvation/')}}/js/jquery.timepicker.min.js"></script>
-		<script src="{{asset('salvation/')}}/js/owl.carousel.min.js"></script>
-		<script src="{{asset('salvation/')}}/js/jquery.magnific-popup.min.js"></script>
-		<script src="{{asset('salvation/')}}/js/scrollax.min.js"></script>
+		<script src="{{asset('salvation/js/jquery.min.js')}}"></script>
+		<script src="{{asset('salvation/js/jquery-migrate-3.0.1.min.js')}}"></script>
+		<script src="{{asset('salvation/js/popper.min.js')}}"></script>
+		<script src="{{asset('salvation/js/bootstrap.min.js')}}"></script>
+		<script src="{{asset('salvation/js/jquery.easing.1.3.js')}}"></script>
+		<script src="{{asset('salvation/js/jquery.waypoints.min.js')}}"></script>
+		<script src="{{asset('salvation/js/jquery.stellar.min.js')}}"></script>
+		<script src="{{asset('salvation/js/jquery.animateNumber.min.js')}}"></script>
+		<script src="{{asset('salvation/js/bootstrap-datepicker.js')}}"></script>
+		<script src="{{asset('salvation/js/jquery.timepicker.min.js')}}"></script>
+		<script src="{{asset('salvation/js/owl.carousel.min.js')}}"></script>
+		<script src="{{asset('salvation/js/jquery.magnific-popup.min.js')}}"></script>
+		<script src="{{asset('salvation/js/scrollax.min.js')}}"></script>
 		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-		<script src="{{asset('salvation/')}}/js/google-map.js"></script>
-		<script src="{{asset('salvation/')}}/js/main.js"></script>
+		<script src="{{asset('salvation/js/google-map.js')}}"></script>
+		<script src="{{asset('salvation/js/main.js')}}"></script>
 		
 	</body>
 	</html>
