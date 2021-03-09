3b3b594aeffa8c04c64a6d235fc65910e7763d27
kevinanggaaa <60419316+kevinanggaaa@users.noreply.github.com>
2021-03-09 17:24:09 +0700

    fix Landing Page Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,22 +20,6 @@
             </div>
         </div>
         @endforeach
-
-        <div class="slider-item js-fullheight" style="background-image:url(salvation/images/bg_2.jpg);">
-            <div class="overlay"></div>
-            <div class="container">
-                <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
-                    <div class="col-md-8 ftco-animate">
-                        <div class="text mt-md-5 w-100 text-center">
-                            <h2>Welcome to PMK ITS</h2>
-                            <h1 class="mb-3">Perkumpulan Mahasiswa Kristen ITS</h1>
-                            <p class="mb-4 pb-3">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in.</p>
-                            <p class="mb-0"><a href="#" class="btn btn-primary py-3 px-2 px-md-4">Become A Volunteer</a></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </section>
 
